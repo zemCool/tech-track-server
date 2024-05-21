1bd70a268dea6cab8a35eb22042ac91468bc39cc
Aziz <trapp360x@gmail.com>
2024-05-21 13:04:12 +0600

    roles permissions

--- routes/euipmentRouter.js
+++ routes/euipmentRouter.js
@@ -1,11 +1,12 @@
 const Router = require('express')
 const router = new Router()
 const equipmentController = require('../controllers/euipmentController');
+const checkRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', equipmentController.create)
+router.post('/', checkRole('ADMIN'), equipmentController.create)
 router.get('/', equipmentController.getAll)
 router.get('/:id', equipmentController.getOne)
-router.put('/:id', equipmentController.updateOne)
-router.delete('/:id', equipmentController.deleteOne)
+router.put('/:id', checkRole('ADMIN'), equipmentController.updateOne)
+router.delete('/:id',checkRole('ADMIN'), equipmentController.deleteOne)
 
 module.exports = router
